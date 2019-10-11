55e176f2eabc1e74f4b01eb59aec4f4ab86232e8
dave-yang <daworld.ny@gmail.com>
2019-10-12 00:03:33 +0500

    Disable submit button if no text provided

--- Assets/chat-handler.js
+++ Assets/chat-handler.js
@@ -5,5 +5,15 @@ $(function(){
     chatWrap.animate({
         scrollTop: chatWrap.prop("scrollHeight")
     }, 600);
-    
+
+    $("[name='message']").keyup(function(){
+        var value = $(this).val();
+            $submit = $("button[type='submit']");
+
+        if ($.trim(value) === '') {
+            $submit.addClass('disabled').prop('disabled', true);
+        } else {
+            $submit.removeClass('disabled').prop('disabled', false);
+        }
+    });
 });
